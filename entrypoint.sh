7510f38f79ae3d445ac0ba58479a6d877aab8a03
lihaixin <lihaixin@15099.net>
2021-09-08 08:27:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-
+# portainer容器里没有sh运行环境,所以下面语句不会执行
 # 判断证书是否存在，没有就用自定义
 if [ ! -f "/etc/cert/$DOMAIN/fullchain.crt" ]; then
   /portainer \
